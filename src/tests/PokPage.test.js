042a63a2af95f553c146e20750ff884209813aad
Anna Kuznetsova <Aniuta907email@gmail.com>
2020-02-16 13:58:20 +0300

    Test doesn't work

--- src/tests/PokPage.test.js
+++ src/tests/PokPage.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import PokPage from '../components/PokPage';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+test('renders PokPage content', () => {
+  const { getByText } = render(<PokPage />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
