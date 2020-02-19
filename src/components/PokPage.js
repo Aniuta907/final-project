51e2e4ff884303307278e95ec6f419d7ff4067a0
Anna Kuznetsova <Aniuta907email@gmail.com>
2020-02-20 00:39:52 +0300

    PokPage was changed

--- src/components/PokPage.js
+++ src/components/PokPage.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { store } from '../store';
+import { getPokemonById } from '../actions';
 
 import './PokPage.css';
 
-export function PokPage( pokemon ) {
-	const { name, picture, caught, id, caughtDate } = pokemon; // потрошим покемона из пропсов на переменные
-	return (
-		<React.Fragment>
-			<div className="card-deck-wrapper" className="pokDeckWr">
-				<div className="card-deck">
-					<div className="divImg">
-						<img src={picture} className="pokImg" alt="Card image" />
-					</div>
+export class PokPage extends Component {
+	constructor(props) {
+		super(props);
 
-					<div className="card divInf">
-						<p className="pokName">ID: {id}</p>
-						<p className="pokName">Name: {name}</p>
-						<p className="pokName">Status: {caught ? 'is caught' : 'is free'}</p>
-						{caught ? <p className="pokName">Caught date: {caughtDate}</p> : null}
+		const pokemon = this.props.pokemon;
+		this.pokemon = pokemon;
+		console.log('PokPage', this.pokemon);
+	}
+
+	componentDidMount() {
+		store.dispatch(getPokemonById());
+	}
+
+	render() {
+		const { name, picture, caught, id, caughtDate } = this.pokemon; // потрошим покемона из пропсов на переменные
+		return (
+			<React.Fragment>
+				<div className="card-deck-wrapper" className="pokDeckWr">
+					<div className="card-deck">
+						<div className="divImg">
+							<img src={picture} className="pokImg" alt="Card image" />
+						</div>
+
+						<div className="card divInf">
+							<p className="pokName">ID: {id}</p>
+							<p className="pokName">Name: {name}</p>
+							<p className="pokName">Status: {caught ? 'is caught' : 'is free'}</p>
+							{caught ? <p className="pokName">Caught date: {caughtDate}</p> : null}
+						</div>
 					</div>
 				</div>
-			</div>
-		</React.Fragment>
-	);
+			</React.Fragment>
+		);
+	}
 }
--- src/tests/PokPage.test.js
+++ src/tests/PokPage.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import PokPage from '../components/PokPage';
 
-test('renders PokPage content', () => {
-  const { getByText } = render(<PokPage />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders PokPage content', () => {
+//   const { getByText } = render(<PokPage />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
