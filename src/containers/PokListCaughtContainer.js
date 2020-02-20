ffbe41bee5f495b7c824e6f28c7f129c7b94c8d6
Anna Kuznetsova <Aniuta907email@gmail.com>
2020-02-20 23:32:55 +0300

    Fixed page refresh bugs

--- src/containers/PokListCaughtContainer.js
+++ src/containers/PokListCaughtContainer.js
@@ -2,15 +2,13 @@ import { connect } from 'react-redux';
 
 import { catchPok, saveCurrentPok } from '../actions/actions';
 import { PokList } from '../components/PokList';
-import { receiveAllPokemons } from '../actions/actions';
 
 const mapStateToProps = (state, ownProps) => ({
-    pokemons: state.pokemons.filter((pokemon) => pokemon.caught === true) //  передаем отфильтрованные данные: только пойманных покемонов
+	pokemons: state.caughtedPokemons // передаем отфильтрованные данные: только пойманных покемонов
 });
 const mapDispatchToProps = (dispatch, ownProps) => ({
-    onClick: (id) => dispatch(catchPok(id)), // для поимки покемона
-    saveCurrent: (id) => dispatch(saveCurrentPok(id)), // для записи в стейт текущего покемона
-    receivePokemons: () => dispatch(receiveAllPokemons())
+	onClick: (id) => dispatch(catchPok(id)), // для поимки покемона
+	saveCurrent: (id) => dispatch(saveCurrentPok(id)) // для записи в стейт текущего покемона
 });
 
 export const PokListCaughtContainer = connect(mapStateToProps, mapDispatchToProps)(PokList); // используем тот же компонент
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,46 +1,48 @@
-const initialState = { pokemons: [], currentId: 1, pokemon: {} };
+const initialState = { pokemons: [], currentId: 1, pokemon: {}, caughtedPokemons: [] };
 
 export function reducer(state = initialState, action) {
-    let newState = {};
-    switch (action.type) {
-        case 'CATCH': // action: поймали какого-то покемона, должны в стейте изменить у этого покемона caught на true
-            const pokemons = [...state.pokemons];
-            const index = pokemons.findIndex((pokemon) => pokemon.id === action.id);
-            pokemons[index].caught = true;
-            pokemons[index].caughtDate = new Date().toLocaleString();
+	let newState = {};
+	switch (action.type) {
+		case 'CATCH': // action: поймали какого-то покемона, должны в стейте изменить у этого покемона caught на true
+			const pokemons = [ ...state.pokemons ];
+			const index = pokemons.findIndex((pokemon) => pokemon.id === action.id);
+			pokemons[index].caught = true;
+			pokemons[index].caughtDate = new Date().toLocaleString();
 
-            newState = {
-                ...state, // сначала копируем старый стейт
-                pokemons: [...pokemons] // потом затираем нужный кусок новыми данными
-            };
+			newState = {
+				...state, // сначала копируем старый стейт
+				pokemons: [ ...pokemons ],
+				caughtedPokemons: [ ...pokemons ].filter((pokemon) => pokemon.caught === true) // потом затираем нужный кусок новыми данными
+			};
 
+			return newState;
+		case 'SAVE_CURRENT': // action: перешли на страницу определенного покемона, должны в id в стейте записать id этого покемона (текщий покемон)
+			newState = {
+				...state,
+				currentId: action.id
+			};
+			return newState;
 
-            return newState;
-        case 'SAVE_CURRENT': // action: перешли на страницу определенного покемона,  должны в id в стейте записать id этого покемона (текщий покемон)
-            newState = {
-                ...state,
-                currentId: action.id
-            };
-            return newState;
+		case 'GET_ALL_POKEMONS':
+			return { ...state, pokemons: action.pokemons };
 
-        case 'GET_ALL_POKEMONS':
+		case 'ADD_PICTURES':
+			return {
+				...state,
+				pokemons: Object.assign({}, state).pokemons.map((pok, i) => ({
+					...pok,
+					picture: i < action.picLength ? action.images[i] : action.images[0],
+					caught: false,
+					caughtDate: undefined
+				}))
+			};
 
-            return {...state, pokemons: action.pokemons };
-
-        case 'ADD_PICTURES':
-            return {
-                ...state,
-                pokemons: Object.assign({}, state).pokemons.map((pok, i) => ({
-                    ...pok,
-                    picture: i < action.picLength ? action.images[i] : action.images[0],
-                    caught: false,
-                    caughtDate: undefined
-                }))
-            };
-
-        case 'GET_POKEMON_BY_ID':
-            return {...state, pokemon: Object.assign({}, state).pokemons.find((pokemon) => pokemon.id === state.currentId) };
-        default:
-            return state;
-    }
+		case 'GET_POKEMON_BY_ID':
+			return {
+				...state,
+				pokemon: Object.assign({}, state).pokemons.find((pokemon) => pokemon.id === state.currentId)
+			};
+		default:
+			return state;
+	}
 }
