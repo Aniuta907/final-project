a3e065225a63aafd45b8f247c7ef4b2dbf7314b3
Anna Kuznetsova1 <anna_kuznetsova1@epam.com>
2021-02-04 16:28:21 +0300

    add toggle to change the theme

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -8,8 +8,13 @@ import './PokemonList.css';
 const pokemonNumberPerPage = 8;
 
 export class PokemonList extends Component {
-	state = {
-		selectedPage: 0
+	constructor (props) {
+		super(props);
+		this.state = {
+			selectedPage: 0,
+			currentTheme: "dark"
+		};
+		this.changeTheme = this.changeTheme.bind(this);
 	};
 
 	handlePageClicked = (data) => {
@@ -19,14 +24,24 @@ export class PokemonList extends Component {
 		});
 	};
 
+	changeTheme() {
+		let theme = this.state.currentTheme === 'dark' ? 'light' : 'dark'
+		this.setState({
+			currentTheme: theme
+		})
+	}
+
 	render() {
 		const { pokemons = [], onClick, saveCurrent } = this.props;
 		const pokemonsForOnePage = pokemons.slice(this.state.selectedPage * pokemonNumberPerPage, this.state.selectedPage * pokemonNumberPerPage + 8);
 		return (
-			<React.Fragment>
+			<div className={ this.state.currentTheme === 'dark' ? 'dark-wrapper' : 'light-wrapper' }>
 				{pokemons.length !== 0 ? (
 					<Pagination handlePageClick={this.handlePageClicked} pageCount={Math.ceil(pokemons.length / 8)} />
 				) : null}
+  				<button onClick={this.changeTheme} className={this.state.currentTheme === 'dark' ? 'btn btn-light' : 'btn btn-secondary' }>
+					Change theme
+				</button>
 				<div className="card-deck-wrapper pokDeckWr">
 					<div className="card-deck pokCardDeck">
 						{pokemonsForOnePage.map((pokemon) => (
@@ -35,11 +50,12 @@ export class PokemonList extends Component {
 								key={pokemon.id}
 								onClick={onClick}
 								saveCurrent={saveCurrent}
+								currentTheme={this.state.currentTheme}
 							/>
 						))}
 					</div>
 				</div>
-			</React.Fragment>
+			</div>
 		);
 	}
 }
